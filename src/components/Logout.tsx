6b43fcc7d614381718ccbc7412bb482926fd78d1
Talha-on-Git <talhayousuf107@gmail.com>
2024-10-10 12:23:07 -0700

    Fixing more errors

--- src/components/Logout.tsx
+++ src/components/Logout.tsx
@@ -1,10 +1,6 @@
 const Logout = () => {
-    let isLoggedIn:boolean = localStorage.getItem('login') === 'true'
-    const handleLogout = () => {
-        isLoggedIn = localStorage.getItem('login') === 'false'
-    }
     return(
-    <button onClick={handleLogout} className="absolute right-6 border-white text-white border-2 p-2 rounded">Logout</button>
+    <button className="absolute right-6 border-white text-white border-2 p-2 rounded">Logout</button>
     )
 }
 
